1c52d3836a73326c16c631858841e410d4995529
Shivashish Yadav <shivashishmca@gmail.com>
2022-06-21 15:42:58 +0530

    initial

---
+++ static/js/90.22be8051.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkrenesaas=self.webpackChunkrenesaas||[]).push([[90],{90:function(n,e,u){u.r(e),u.d(e,{default:function(){return s}});var a=u(7313),r=u(7030);function s(){var n=(0,r.TH)().pathname;return(0,a.useEffect)((function(){window.scrollTo(0,0)}),[n]),null}}}]);
